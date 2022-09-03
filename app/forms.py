f5888ef062c7028c152b0c452188f03f9fbc0663
Stephen Gilmore <stephen@gilmore.us>
2022-09-03 15:06:12 -0700

    finished ch 6

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 
 from app.models import User
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
 
 
 class LoginForm(FlaskForm):
