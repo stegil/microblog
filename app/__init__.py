5bef9169fb2200c85d7d2fae621baeeee0182ea7
Stephen Gilmore <stephen@gilmore.us>
2022-09-03 13:49:19 -0700

    completed ch 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
+
 
 app = Flask(__name__)
+
+# Configuration
 app.config.from_object(Config)
 
+# flask-sqlalchemy & flask-migrate
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# flask-login
+login = LoginManager(app)
+login.login_view = "login"  # redirect if not logged in
+
 
 from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +11,33 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField(
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Register")
+
+    # Flask-WTF automatically adds anything matching validate_{{ field name }}
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(
+                "Please choose a different username. {} is already taken".format(
+                    username.data
+                )
+            )
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(
+                "Please choose a different email. {} is already taken".format(
+                    email.data
+                )
+            )
